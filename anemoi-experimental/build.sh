66a56b38995939c4f1ac3aaa66fcaf3c39257d02
dietervdb-meteo <dieeter.vandenbleeken@meteo.be>
2024-10-14 16:13:46 +0300

    update to rocm 6.2 and forcing area

--- anemoi-experimental/build.sh
+++ anemoi-experimental/build.sh
@@ -4,10 +4,12 @@ set -e
 
 # Use latest modules 
 # check if the below is up to date and necessary
+module purge
 module use /appl/local/training/modules/AI-20240529
-module load LUMI/24.03 cotainr
+module load LUMI/24.03 cotainr/2023.11.0.1-cray-python-3.9.13.1
 
-CONTAINER=anemoi_lam #anemoi_std
+#CONTAINER=anemoi_lam 
+CONTAINER=anemoi_std
 # Directories
 d_WORK=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 d_CONDATOOLS=/scratch/project_465001235/vandenbl/containers/tools/Conda-Tools
@@ -17,7 +19,7 @@ printf -v date '%(%Y%m%d)T'
 
 # Files
 f_ENV=$CONTAINER-recipe.yml
-f_BASE=lumi-rocm-rocm-5.6.1.sif
+f_BASE=/appl/local/containers/sif-images/lumi-rocm-rocm-6.2.2.sif
 f_SIF=${CONTAINER}_${date}.sif
 f_INST=${CONTAINER}_${date}-installed.yml
 
@@ -26,7 +28,6 @@ cd $d_WORK
 # Remove previous container
 rm -f $f_SIF
 echo "let's build a container"
-
 cotainr build $f_SIF --base-image=$f_BASE --conda-env=$f_ENV --accept-licenses
 
 echo 'container finished'
