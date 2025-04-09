3cc373e892606ee41eb3f4020b7e0f22ef7894ca
dietervdb-meteo <dieter.vandenbleeken@meteo.be>
2025-04-09 14:28:56 +0300

    add some use cases of dieter

---
+++ anemoi-rebase/build.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+set -e
+
+# Use latest modules 
+# check if the below is up to date and necessary
+module purge
+module use /appl/local/training/modules/AI-20241126
+module load LUMI/24.03 cotainr/2023.11.0-20240909
+
+CONTAINER=anemoi-base-xesmf
+
+# Directories
+d_WORK=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+d_CONDATOOLS=/scratch/project_465001235/vandenbl/containers/tools/Conda-Tools
+d_CONT=/scratch/project_465001235
+
+printf -v date '%(%Y%m%d)T'
+
+# Files
+f_ENV=$CONTAINER-recipe.yml
+f_BASE=/appl/local/containers/sif-images/lumi-rocm-rocm-6.2.2.sif
+#f_BASE=/scratch/project_465001235/containers/anemoi-base/anemoi-base.sif
+#f_BASE=/appl/local/containers/sif-images/lumi-rocm-rocm-5.7.3.sif
+f_SIF=${CONTAINER}_${date}.sif
+f_INST=${CONTAINER}${date}-installed.yml
+
+cd $d_WORK
+
+# Remove previous container
+rm -f $f_SIF
+echo "let's build a container"
+cotainr build $f_SIF --base-image=$f_BASE --conda-env=$f_ENV --accept-licenses
+
+echo 'container finished'
+
+echo 'inspecting what we have build'
+CMD="python ${d_CONDATOOLS}/conda_export.py --from-history --use-versions -o $f_INST"
+singularity exec -B $d_CONT $f_SIF bash -c "$CMD"
+
+echo 'output written to '$f_INST
